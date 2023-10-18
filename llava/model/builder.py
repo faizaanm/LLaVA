2ab78eb674cc39d2a10f8089dbe0457349a14921
Faizaan Merchant <fizz321@gmail.com>
2023-10-18 16:05:41 -0700

    add cache

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -99,8 +99,8 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
                 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
                 model = LlavaMPTForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
             else:
-                tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
-                model = LlavaLlamaForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
+                tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, cache_dir="/cache")
+                model = LlavaLlamaForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, cache_dir="/cache", **kwargs)
     else:
         # Load language model
         if model_base is not None:
